5e090867017ae0d5de5d54e97a40c7aa08c7e00d
Ari Schermuk <arielschermuk@gmail.com>
2022-10-12 18:37:13 -0300

    initial commit

---
+++ src/main/java/ort/edu/futbolTinder/FutbolTinderApiApplication.java
@@ -0,0 +1,13 @@
+package ort.edu.futbolTinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolTinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolTinderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ort/edu/futbolTinder/FutbolTinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ort.edu.futbolTinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolTinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
