45bcb4dc5fa379093055be1aefea74330d85a7ec
Ari Schermuk <arielschermuk@gmail.com>
2022-10-26 19:16:49 -0300

    add repository beans

--- src/main/java/ort/edu/futbolTinder/config/RepositoryConfig.java
+++ src/main/java/ort/edu/futbolTinder/config/RepositoryConfig.java
@@ -1,14 +1,34 @@
 package ort.edu.futbolTinder.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import ort.edu.futbolTinder.partido.entity.Partido;
 import ort.edu.futbolTinder.partido.repository.PartidoRepository;
+import ort.edu.futbolTinder.partido.repository.PartidoRepositoryJsonImpl;
 import ort.edu.futbolTinder.partido.repository.PartidoRepositoryLocalImpl;
 
+import javax.persistence.EntityManager;
+
 @Configuration
+@RequiredArgsConstructor
 public class RepositoryConfig {
-    @Bean
-    PartidoRepository partidoRepository() {
+    EntityManager entityManager;
+
+    @Bean("local")
+    PartidoRepository partidoLocalRepository() {
         return new PartidoRepositoryLocalImpl();
+    }
+
+    @Bean("json")
+    PartidoRepository partidoJsonRepository() {
+        return new PartidoRepositoryJsonImpl();
+    }
+
+    @Bean("db")
+    JpaRepository<Partido, Long> partidoDBRepository() {
+        return new SimpleJpaRepository<>(Partido.class, entityManager);
     }
 }
--- src/main/java/ort/edu/futbolTinder/partido/service/PartidoService.java
+++ src/main/java/ort/edu/futbolTinder/partido/service/PartidoService.java
@@ -1,13 +1,13 @@
 package ort.edu.futbolTinder.partido.service;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ort.edu.futbolTinder.generic.service.CRUDService;
 import ort.edu.futbolTinder.partido.dto.request.PartidoRequestDTO;
 import ort.edu.futbolTinder.partido.dto.response.PartidoDTO;
 import ort.edu.futbolTinder.partido.entity.Partido;
 import ort.edu.futbolTinder.partido.repository.PartidoRepository;
-import ort.edu.futbolTinder.partido.repository.PartidoRepositoryLocalImpl;
 
 import javax.persistence.EntityManager;
 
@@ -15,7 +15,7 @@ import static ort.edu.futbolTinder.utils.mapping.MapperUtils.setIfNotNull;
 
 @Service
 public class PartidoService extends CRUDService<PartidoDTO, Partido, PartidoRequestDTO> {
-    public PartidoService(PartidoRepository repository, EntityManager entityManager, ModelMapper modelMapper) {
+    public PartidoService(@Qualifier("local") PartidoRepository repository, EntityManager entityManager, ModelMapper modelMapper) {
         super(repository, entityManager, modelMapper, Partido.class, PartidoDTO.class);
     }
 
