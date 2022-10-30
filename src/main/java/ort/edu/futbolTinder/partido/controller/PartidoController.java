5aa8aca1da60c39fe58cee9b9e3474caf4cc28f5
Ari Schermuk <arielschermuk@gmail.com>
2022-10-30 13:37:21 -0300

    match endpoint (returns getAll)

--- src/main/java/ort/edu/futbolTinder/partido/controller/PartidoController.java
+++ src/main/java/ort/edu/futbolTinder/partido/controller/PartidoController.java
@@ -1,17 +1,31 @@
 package ort.edu.futbolTinder.partido.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ort.edu.futbolTinder.generic.controller.CRUDController;
-import ort.edu.futbolTinder.generic.service.CRUDService;
 import ort.edu.futbolTinder.partido.dto.request.PartidoRequestDTO;
 import ort.edu.futbolTinder.partido.dto.response.PartidoDTO;
 import ort.edu.futbolTinder.partido.service.PartidoService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/partidos")
 public class PartidoController extends CRUDController<PartidoDTO, PartidoRequestDTO> {
-    public PartidoController(PartidoService crudService) {
-        super(crudService);
+
+    private final PartidoService partidoService;
+
+    public PartidoController(PartidoService partidoService) {
+        super(partidoService);
+        this.partidoService = partidoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PartidoDTO>> match(@RequestParam double longitude, @RequestParam double latitude) {
+        return new ResponseEntity<>(partidoService.findMatchCandidates(longitude, latitude), HttpStatus.OK);
     }
 }
--- src/main/java/ort/edu/futbolTinder/partido/service/PartidoService.java
+++ src/main/java/ort/edu/futbolTinder/partido/service/PartidoService.java
@@ -7,9 +7,10 @@ import ort.edu.futbolTinder.partido.dto.request.PartidoRequestDTO;
 import ort.edu.futbolTinder.partido.dto.response.PartidoDTO;
 import ort.edu.futbolTinder.partido.entity.Partido;
 import ort.edu.futbolTinder.partido.repository.PartidoRepository;
-import ort.edu.futbolTinder.partido.repository.PartidoRepositoryLocalImpl;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.function.Function;
 
 import static ort.edu.futbolTinder.utils.mapping.MapperUtils.setIfNotNull;
 
@@ -22,5 +23,13 @@ public class PartidoService extends CRUDService<PartidoDTO, Partido, PartidoRequ
     @Override
     protected void setEntityFieldsFromDTO(PartidoRequestDTO partidoRequestDTO, Partido partido) {
         setIfNotNull(partidoRequestDTO.getCancha(), partido::setCancha);
+    }
+
+    public List<PartidoDTO> findMatchCandidates(double longitude, double latitude) {
+        return super.getAll(null);
+    }
+
+    private Function<Partido, PartidoDTO> getPartidoPartidoDTOFunction() {
+        return p -> modelMapper.map(p, PartidoDTO.class);
     }
 }
