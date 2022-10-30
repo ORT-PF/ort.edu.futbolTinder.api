6228dd052db2740b6b27fce37f9be8d07eb90d09
Ari Schermuk <arielschermuk@gmail.com>
2022-10-30 13:46:15 -0300

    definition of other beans

--- src/main/java/ort/edu/futbolTinder/config/RepositoryConfig.java
+++ src/main/java/ort/edu/futbolTinder/config/RepositoryConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ort.edu.futbolTinder.partido.entity.Partido;
+import ort.edu.futbolTinder.partido.repository.PartidoRepositoryJsonImpl;
 import ort.edu.futbolTinder.partido.repository.PartidoRepositoryLocalImpl;
 
 @Configuration
 public class RepositoryConfig {
     @Bean("local")
-    JpaRepository<Partido, Long> partidoRepository() {
+    JpaRepository<Partido, Long> partidoLocalRepository() {
         return new PartidoRepositoryLocalImpl();
+    }
+
+    @Bean("json")
+    JpaRepository<Partido, Long> partidoJsonRepository() {
+        return new PartidoRepositoryJsonImpl();
     }
 }
