a1c3043fea0c02c88a282e74b096c8b458be0dee
Ari Schermuk <arielschermuk@gmail.com>
2022-11-02 13:04:34 -0300

    add postrgresql persistance

--- src/main/java/ort/edu/futbolTinder/repository/PartidoRepository.java
+++ src/main/java/ort/edu/futbolTinder/repository/PartidoRepository.java
@@ -1,9 +1,11 @@
 package ort.edu.futbolTinder.repository;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ort.edu.futbolTinder.entity.Partido;
 
 @Repository
+@Qualifier("jpa")
 public interface PartidoRepository extends JpaRepository<Partido, Long> {
 }
