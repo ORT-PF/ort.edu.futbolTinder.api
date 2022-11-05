82df38000d2ce07b170f6a7a3d7d9b42366384a9
Ari Schermuk <arielschermuk@gmail.com>
2022-11-05 16:42:02 -0300

    create match endpoint, some filters are WIP

--- src/main/java/ort/edu/futbolTinder/dto/response/MatchCandidateDTO.java
+++ src/main/java/ort/edu/futbolTinder/dto/response/MatchCandidateDTO.java
@@ -3,22 +3,20 @@ package ort.edu.futbolTinder.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PartidoDTO {
+public class MatchCandidateDTO {
     private Long id;
     private Long hostId;
     private String fieldName;
     private String fieldAddress;
     private LocalDateTime dateTime;
-    private Integer originalQuota;
     private Integer remainingQuota;
-    private List<Long> joinedPlayers;
     private Double longitude;
     private Double latitude;
-
+    private Double distance;
 }
